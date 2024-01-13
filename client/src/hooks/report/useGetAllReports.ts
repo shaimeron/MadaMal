4fe5e246f603f6db1cb071f9cf2f5bf00bce25e3
galronen10 <galronen10@gmail.com>
2024-01-13 19:44:40 +0200

    add polling to client

--- client/src/hooks/report/useGetAllReports.ts
+++ client/src/hooks/report/useGetAllReports.ts
@@ -2,14 +2,21 @@ import { useEffect } from "react"
 import { api } from "../../api"
 import { setReports } from "../../store/reports"
 import { useDispatch } from "react-redux"
+import { useQuery } from "@tanstack/react-query";
 
+const refetchInterval = 3000;
+ 
 export const useGetAllReports = async () => {
-    const dispatch = useDispatch()
-    useEffect(() => { 
-        const fetchFunc = async () => {
-            const data  = await api.report.getAll()
-        dispatch(setReports(data.map(report => ({...report, creationDate: new Date(report.creationDate)}))))
-        }
-        fetchFunc()
-    },[dispatch])
+    const dispatch = useDispatch();
+    const {data } = useQuery({
+        queryKey: ['allReports'],
+        queryFn: async () => {
+          const data  = await api.report.getAll()
+          return data.map(report => ({...report, creationDate: new Date(report.creationDate)}))
+        },
+        refetchInterval: refetchInterval,
+      })
+
+      dispatch(setReports(data ?? []))
+
 }
