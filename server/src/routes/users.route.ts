b98af5af7067f7b45c1589e0db88b65116688107
harelush <harelchen1999@gmail.com>
2024-01-21 17:11:08 +0200

    feature: added jwt to client

---
+++ server/src/routes/users.route.ts
@@ -0,0 +1,32 @@
+import express from "express";
+const router = express.Router();
+import { ReportsController } from "../controllers";
+
+const reportsController = new ReportsController();
+
+router.get("/all", reportsController.getAll.bind(reportsController));
+
+router.get("/:id", reportsController.getById.bind(reportsController));
+
+router.post("/", reportsController.createReport.bind(reportsController));
+
+router.put("/", reportsController.updateReport.bind(reportsController));
+
+router.delete("/:id", reportsController.deleteById.bind(reportsController));
+
+router.post(
+  "/update",
+  reportsController.addUpdateToReport.bind(reportsController)
+);
+
+router.delete(
+  "/update/:reportId/:updateId",
+  reportsController.deleteUpdateFromReport.bind(reportsController)
+);
+
+router.put(
+  "/update",
+  reportsController.changeUpdateInReport.bind(reportsController)
+);
+
+export default router;
