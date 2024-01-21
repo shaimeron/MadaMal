4bd9d795233ea2ea152b26e1a2489ee23fea5d7a
harelush <harelchen1999@gmail.com>
2024-01-21 17:17:13 +0200

    feature: added auth to get user route

--- server/src/routes/users.route.ts
+++ server/src/routes/users.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import {UsersController} from "../controllers";
+import {authMiddleware} from "../common";
 
 const router = express.Router();
 
 const usersController = new UsersController();
 
-router.get("/:id", usersController.getById.bind(usersController));
+router.get("/:id", authMiddleware, usersController.getById.bind(usersController));
 
 export default router;
