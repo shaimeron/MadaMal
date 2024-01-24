1cb350e2c05c28f1a82938cf02a7b240a17c18ae
Shay Meiron <smeiron@quotient.com>
2024-01-24 20:37:00 +0200

    server: add swagger and swagger doc to each route

--- server/src/routes/auth_route.ts
+++ server/src/routes/auth_route.ts
@@ -2,9 +2,88 @@ import express from "express";
 const router = express.Router();
 import authController from "../controllers/auth_controller";
 
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Register a new user.
+ *     description: Creates a new user account.
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       description: User details.
+ *     responses:
+ *       200:
+ *         description: User created successfully.
+ *         content:
+ *           application/json:
+ *             example:
+ *               email: awesome@gmail.com
+ *               password: pass123
+ *               fullname: Awesome User
+ *               imageUrl: https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png
+ *       400:
+ *         description: Bad request. User details are invalid.
+ *       500:
+ *         description: Internal server error.
+ */
 router.post("/register", authController.register);
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Login.
+ *     description: Logs a user into the system.
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       description: User credentials.
+ *       content:
+ *         application/json:
+ *           example:
+ *             username: user123
+ *             password: pass123
+ *     responses:
+ *       200:
+ *         description: User logged in successfully.
+ *       400:
+ *         description: Bad request. User details are invalid.
+ *       500:
+ *         description: Internal server error.
+ */
 router.post("/login", authController.login);
+
+/**
+ * @swagger
+ * /logout:
+ *   get:
+ *     summary: Logout.
+ *     description: Logs a user out of the system.
+ *     tags:
+ *       - Authentication
+ *     responses:
+ *       200:
+ *         description: User logged out successfully.
+ *       500:
+ *         description: Internal server error.
+ */
 router.get("/logout", authController.logout);
+
+/**
+ * @swagger
+ * /refresh:
+ *   get:
+ *     summary: Refresh token.
+ *     description: Refreshes a user's token.
+ *     tags:
+ *       - Authentication
+ *     responses:
+ *       200:
+ *         description: Token refreshed successfully.
+ *       500:
+ *         description: Internal server error.
+ */
 router.get("/refresh", authController.refresh);
 
 export default router;
--- server/src/routes/users.route.ts
+++ server/src/routes/users.route.ts
@@ -6,6 +6,32 @@ const router = express.Router();
 
 const usersController = new UsersController();
 
+/**
+ * @swagger
+ * /{id}:
+ *   get:
+ *     summary: Retrieve a user by their ID.
+ *     description: Fetches a specific user's details by their unique identifier. Requires authentication.
+ *     tags:
+ *       - Users
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         type: string
+ *         description: Unique identifier of the user to retrieve.
+ *     responses:
+ *       200:
+ *         description: User details retrieved successfully.
+ *       401:
+ *         description: Unauthorized. Token is missing or invalid.
+ *       404:
+ *         description: User not found.
+ *       500:
+ *         description: Internal server error.
+ */
 router.get("/:id", authMiddleware, usersController.getById.bind(usersController));
 
 export default router;
