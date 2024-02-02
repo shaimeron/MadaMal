902e16c1a192d14b7d85083d098b98ac33092101
galronen10 <galronen10@gmail.com>
2024-02-02 17:24:58 +0200

    separate socket io to file and fix cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,48 +1,13 @@
 import initApp from "./app";
 // import https from "https";
 import http from "http";
-import { Server } from "socket.io";
-import { messageModel } from "./models/messages/messages.model";
+import { startSocketIO } from "./socketIO";
 
 initApp().then((app) => {
   // if (process.env.NODE_ENV !== 'production') {
   // console.log('development');
   const server = http.createServer(app);
-  const socketIO = new Server(server, {
-    cors: {
-      origin: "*", // Replace with your client app's URL
-      methods: ["GET", "POST"],
-    },
-  });
-
-  socketIO.on("connection", (socket) => {
-    console.log("New client connected");
-    socket.on("request chat history", () => {
-      messageModel
-        .find()
-        .sort({ timestamp: 1 })
-        .limit(50)
-        .exec()
-        .then((messages) => {
-          socket.emit("chat history", messages);
-        })
-        .catch((err) => {
-          console.error("Error fetching chat history:", err);
-        });
-    });
-    socket.on("chat message", (data) => {
-      const newMessage = new messageModel({
-        username: data.username,
-        message: data.message,
-      });
-      newMessage.save();
-      socketIO.emit("chat message", data);
-    });
-
-    socket.on("disconnect", () => {
-      console.log("Client disconnected");
-    });
-  });
+  startSocketIO(server);
 
   server.listen(process.env.PORT);
   // }
