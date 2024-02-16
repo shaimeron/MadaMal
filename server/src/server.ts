2796ed2f461c9bf050cc8816a25d958ed69b6bf8
galronen10 <galronen10@gmail.com>
2024-02-16 16:54:58 +0200

    add https to server when run in production

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,20 +7,19 @@ import { startSocketIO } from "./socketIO";
 initApp().then((app) => {
   let server: http.Server | https.Server;
 
-  // if (process.env.NODE_ENV !== "production") {
-  console.log("development");
-  server = http.createServer(app);
-  server.listen(process.env.PORT);
-  // } else {
-  //   console.log("PRODUCTION");
-
-  //   const httpsOptions = {
-  //     key: fs.readFileSync("../client-key.pem"),
-  //     cert: fs.readFileSync("../client-cert.pem"),
-  //   };
-  //   server = https.createServer(httpsOptions, app);
-  //   server.listen(process.env.HTTPS_PORT);
-  // }
+  if (process.env.NODE_ENV !== "production") {
+    console.log("development");
+    server = http.createServer(app);
+    server.listen(process.env.PORT);
+  } else {
+    console.log("PRODUCTION");
+    const options2 = {
+      key: fs.readFileSync("./client-key.pem"),
+      cert: fs.readFileSync("./client-cert.pem"),
+    };
+    server = https.createServer(options2, app);
+    server.listen(process.env.HTTPS_PORT);
+  }
 
   startSocketIO(server);
 });
