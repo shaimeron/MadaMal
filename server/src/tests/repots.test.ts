323586c44bb4357b3191e319290861155be471dc
Shay Meiron <smeiron@quotient.com>
2024-02-18 18:39:22 +0200

    server: add update test

--- server/src/tests/repots.test.ts
+++ server/src/tests/repots.test.ts
@@ -3,7 +3,7 @@ import initApp from "../app";
 import mongoose from "mongoose";
 import { Express } from "express";
 import { ReportsModel } from "../models/reports"; // Make sure to import your ReportsModel interface
-import { IReportDTO, IUpdateInReportDTO } from "../models/reports"; // Import your DTO interfaces
+import { IReportDTO } from "../models/reports"; // Import your DTO interfaces
 
 let app: Express;
 let reportId: string;
@@ -33,6 +33,21 @@ describe("ReportsController tests", () => {
     expect(response.statusCode).toBe(201);
     // Add assertions for the response body as needed
     reportId = response.body._id;
+  });
+
+  test("Test updateReport", async () => {
+    const reportData: IReportDTO = {
+      _id: reportId,
+      data: "Updated test data",
+      imageName: "Updated test image name",
+      ownerId: "5e4ba1f05717192b9c565321",
+    };
+
+    const response = await request(app)
+      .put("/reports")
+      .send(reportData);
+
+    expect(response.statusCode).toBe(201);
   });
 
   // Add tests for other methods like updateReport, deleteById, addUpdateToReport, changeUpdateInReport, and deleteUpdateFromReport
